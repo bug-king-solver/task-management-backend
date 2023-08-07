d920323d985a245c1a380a718a8e1f2c7cbf9138
alen <fation.qama.god@gmail.com>
2023-08-07 07:32:24 +0200

    add json-server write

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const jsonServer = require('json-server')
+const path = require('path')
+const fs = require('fs')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
